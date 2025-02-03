52b3b132a8ea59414677dab4def9b7e1dc67e1d3
Lorenzo <lorenzo.piarulli3000@gmail.com>
2025-02-03 12:53:53 +0100

    Alcune modifiche hackathon

--- tester.py
+++ tester.py
@@ -1,9 +1,8 @@
 
 import subprocess
-import pandas as pd
+#import pandas as pd
 
-command = ["mpirun", "-np", "4", "mpi_script.exe", ">", "output.txt", "2>", "error.txt"]
-command_win = ["mpiexec", "-np", "4", "jacobi_method.exe"]
+command = ["mpirun", "-np", "4", "jacobi_method"]
 
 # Vedere come i codici di saverio prendono input, in caso modificarli per parsare input da .csv
 
@@ -12,16 +11,23 @@ command_win = ["mpiexec", "-np", "4", "jacobi_method.exe"]
 # Leggere il file di output prodotto da mpirun
 
 def executeMPI():
-
-
     for i in range(5):
-        try:
-            result = subprocess.run(command_win, check=True, capture_output=True, text=True)
-            print("Output:\n", result.stdout)
-        except subprocess.CalledProcessError as e:
-            print("Errore nell'esecuzione di mpirun:", e.stderr)
+        with open("output", "w") as outfile, open("public_results", "r") as results:
+            try:
+                result = subprocess.run(command, stdout=outfile,  stderr=subprocess.STDOUT, text=True)
+
+                # PARSE OUTFILE
+
+                # PARSE RESULTS
+
+                # COMPARE THEM
+
+                print(f"Test {i} DONE!") 
+            except subprocess.CalledProcessError as e:
+                print("Errore nell'esecuzione di mpirun:", e.stderr)
+
+
 
 if __name__ == '__main__':
     print("TESTING")
-
     executeMPI()
